327980321d5a58573161de25877e7ac4eb254109
Paul FICOT <33148717+PaulFICOT@users.noreply.github.com>
2019-01-16 15:38:33 +0100

    16012019_1

---
+++ Phase 4/Arduino/Arduino.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // mettez votre code d'installation ici, pour lancer une fois:
+   Serial.begin (9600);
+}
+
+void loop() {
+  // mettez votre code principal ici, pour exécuter à plusieurs reprises:
+  Serial.println("Hellow world");
+  Serial.println("TEST");
+  Serial.println("End");
+}
